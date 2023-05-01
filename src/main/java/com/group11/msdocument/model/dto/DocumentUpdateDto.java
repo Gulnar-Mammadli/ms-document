1cc55054c7c3b9b497d5bead625f331d8797e31f
mammadli <gulnar.mammadli@ut.ee>
2023-05-01 23:24:51 +0300

    Add DocumentUpdateDto

---
+++ src/main/java/com/group11/msdocument/model/dto/DocumentUpdateDto.java
@@ -0,0 +1,35 @@
+package com.group11.msdocument.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.group11.msdocument.model.enums.DocumentType;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class DocumentUpdateDto {
+
+    @NotNull
+    @JsonProperty("loan_id")
+    private Long loanId;
+
+    @NotNull
+    @JsonProperty("customer_id")
+    private Long customerId;
+
+    @NotNull
+    @JsonProperty("document_type")
+    private DocumentType documentType;
+
+    @NotNull
+    @JsonProperty("approved_by")
+    private Long approvedBy;
+
+}
