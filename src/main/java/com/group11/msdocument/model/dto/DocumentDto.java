0d9b58cf370af5bf29af1c680d25a27c3b0e8e78
mammadli <gulnar.mammadli@ut.ee>
2023-05-01 23:28:27 +0300

    Add not null constraint

--- src/main/java/com/group11/msdocument/model/dto/DocumentDto.java
+++ src/main/java/com/group11/msdocument/model/dto/DocumentDto.java
@@ -2,15 +2,12 @@ package com.group11.msdocument.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.group11.msdocument.model.enums.DocumentType;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,15 +15,15 @@ import java.time.LocalDateTime;
 @Builder
 public class DocumentDto {
 
+    @NotNull
     @JsonProperty("loan_id")
     private Long loanId;
 
+    @NotNull
     @JsonProperty("customer_id")
     private Long customerId;
 
-    @JsonProperty("approved_by")
-    private Long approvedBy;
-
+    @NotNull
     @JsonProperty("document_type")
     private DocumentType documentType;
 
