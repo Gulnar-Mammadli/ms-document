2c64ea2ec32caa969d3d4d7fee62ab039fbebe16
mammadli <gulnar.mammadli@ut.ee>
2023-05-01 23:29:49 +0300

    Add new queries

--- src/main/java/com/group11/msdocument/repository/DocumentRepository.java
+++ src/main/java/com/group11/msdocument/repository/DocumentRepository.java
@@ -1,13 +1,17 @@
 package com.group11.msdocument.repository;
 
 import com.group11.msdocument.model.Document;
+import com.group11.msdocument.model.enums.DocumentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document,Long> {
 
-    Optional<Document> findByCustomerIdAndLoanId(Long customerId, Long loanId);
+    Optional<Document> findByCustomerIdAndLoanIdAndDocumentType(Long customerId, Long loanId, DocumentType documentType);
+    Optional<List<Document>> findAllByCustomerId(Long customerId);
+
 }
