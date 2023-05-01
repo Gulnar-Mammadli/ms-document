e72da6b8b4bee16b83046dc90b57cdf41af5414d
mammadli <gulnar.mammadli@ut.ee>
2023-05-01 23:29:23 +0300

    Add implementations for endpoints

--- src/main/java/com/group11/msdocument/service/DocumentService.java
+++ src/main/java/com/group11/msdocument/service/DocumentService.java
@@ -1,7 +1,65 @@
 package com.group11.msdocument.service;
 
+import com.group11.msdocument.mapper.DocumentMapper;
+import com.group11.msdocument.model.Document;
+import com.group11.msdocument.model.dto.DocumentDto;
+import com.group11.msdocument.model.dto.DocumentUpdateDto;
+import com.group11.msdocument.repository.DocumentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class DocumentService {
+
+    private final DocumentRepository documentRepository;
+
+    public Document uploadDocument(DocumentDto documentDto) {
+        Optional<Document> result = documentRepository.findByCustomerIdAndLoanIdAndDocumentType(documentDto.getCustomerId(),
+                documentDto.getLoanId(), documentDto.getDocumentType());
+        if (result.isEmpty()) {
+            Document document = DocumentMapper.INSTANCE.mapToDocument(documentDto);
+            document.setCreatedAt(LocalDateTime.now());
+            return documentRepository.save(document);
+        }
+        return null;
+    }
+
+
+//    TODO ancaq approved olmamislar update oluna biler?
+    public Document updateDocument(DocumentUpdateDto documentUpdateDto, Long documentId) {
+        Optional<Document> result = documentRepository.findById(documentId);
+        if (result.isPresent()) {
+            Document document = DocumentMapper.INSTANCE.mapFromDocumentUpdateDto(documentUpdateDto);
+            document.setId(documentId);
+            document.setApproved(true);
+            document.setCreatedAt(result.get().getCreatedAt());
+            document.setUpdatedAt(LocalDateTime.now());
+            return documentRepository.save(document);
+        }
+        return null;
+    }
+
+    public List<Document> getDocumentByCustomerId(Long customerId) {
+        Optional<List<Document>> list = documentRepository.findAllByCustomerId(customerId);
+        return list.orElse(null);
+    }
+
+    public Document getDocumentByDocumentId(Long documentId) {
+        Optional<Document> result = documentRepository.findById(documentId);
+        return result.orElse(null);
+    }
+
+//    TODO approve olunubsa delete oluna biler?
+    public Void deleteDocument(Long document_id) {
+        Optional<Document> result = documentRepository.findById(document_id);
+        if(result.isPresent()){
+            documentRepository.deleteById(document_id);
+        }
+        return null;
+    }
 }
